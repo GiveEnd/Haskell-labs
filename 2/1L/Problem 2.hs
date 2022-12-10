1fa109554c9a01d1423bd9eb252b5c4aed4a1be3
GiveEnd <60320294+GiveEnd@users.noreply.github.com>
2022-12-11 06:49:47 +0800

    Update Problem 2.hs

--- 2/1L/Problem 2.hs
+++ 2/1L/Problem 2.hs
@@ -1,3 +1,3 @@
 fLast :: [a] -> a
 fLast l = last(init(l))
-main = putStrLn(show(fLast [1, 2, 3, 4])++" "++ show(fLast ["a","b","c","d"]))
+main = putStrLn(show(fLast [1..4])++" "++ show(fLast ['a'..'d']))
