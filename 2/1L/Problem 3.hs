93a161a1937f1173b51103e1f9bdcddc531fe06e
GiveEnd <60320294+GiveEnd@users.noreply.github.com>
2022-12-11 06:51:35 +0800

    Update Problem 3.hs

--- 2/1L/Problem 3.hs
+++ 2/1L/Problem 3.hs
@@ -1,3 +1,3 @@
 fLast :: [a]->Int -> a
 fLast l x = last(take x l)
-main = putStrLn(show(fLast [1, 2, 3, 4, 5] 5)++" "++ show(fLast ["a","b","c","d","e"] 5))
+main = putStrLn(show(fLast [1..5] 5)++" "++ show(fLast ['a'..'e'] 5))
