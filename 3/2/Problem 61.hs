b59063c32832e31d95b46063ba570444482c994d
GiveEnd <60320294+GiveEnd@users.noreply.github.com>
2022-12-19 21:58:01 +0800

    Create Problem 61.hs

---
+++ 3/2/Problem 61.hs
@@ -0,0 +1,11 @@
+data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show, Eq)
+
+tree4 = Branch 1 (Branch 2 Empty (Branch 4 Empty Empty))
+                 (Branch 2 Empty Empty)
+
+main =  do print (countLeaves tree4)  
+
+countLeaves :: Tree a -> Int
+countLeaves Empty = 0
+countLeaves (Branch x Empty Empty) = 1
+countLeaves (Branch x ct1 ct2) = countLeaves ct1 + countLeaves ct2
